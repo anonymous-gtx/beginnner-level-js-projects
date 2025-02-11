b160d158d4fde45eacdd0d48d4fa18681e4ca354
anonymous-gtx <anonymous-gtx@proton.me>
2025-02-12 03:50:12 +0530

    keyboard project my version

--- keyboard/logic.js
+++ keyboard/logic.js
@@ -3,18 +3,7 @@ const insert = document.getElementById('insert')
 window.addEventListener('keydown', (e) =>{
     insert.innerHTML = `
     <div class='color'>
-    <table>
-    <tr>
-        <th>Key</th>
-        <th>KeyCode</th>
-        <th>Code</th>
-    </tr>
-    <tr>
-        <td>${e.key === " " ? " Space ": e.key}</td>
-        <td>${e.keyCode}</td>
-        <td>${e.code}</td>
-    </tr>
-    </table>
+    <span>Key Pressed: ${e.key === ' ' ? 'Space' : e.key}</span>
     </div>
     `
 });
